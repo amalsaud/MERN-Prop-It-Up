7aee56f6e04cfff2927476cc4f9dbb7c3cb7bdcd
Amal Aldawsari <93773622+amalsaud@users.noreply.github.com>
2022-08-20 17:24:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 import PersonCard from "./components/PersonCard"
 
 function App() {
+  var personList = [
+    { lName: "Doe", fName: "Jane", age: 45, hairColor: "Black" },
+    { lName: "Smith", fName: "John", age: 88, hairColor: "Brown" },
+    { lName: "Fillmore", fName: "Millard", age: 50, hairColor: "Brown" },
+    { lName: "Smith", fName: "Marie", age: 62, hairColor: "Brown" }
+  ]
   return (
     <div className='App'>
-    <PersonCard lName={"Doe"} fName={"Jane"} age={45} hairColor={"Black"}/>
-    <PersonCard lName={"Smith"} fName={"John"} age={88} hairColor={"Brown"}/>
-    <PersonCard lName={"Fillmore"} fName={"Millard"} age={50} hairColor={"Brown"}/>
-    <PersonCard lName={"Smith"} fName={"Marie"} age={62} hairColor={"Brown"}/>
+      {personList.map(person => <PersonCard
+        lName={"Doe"}
+        fName={"Jane"}
+        age={45}
+        hairColor={"Black"}
+      />
+      )}
     </div>
   );
 }
