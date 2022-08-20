0066545678b3775a787f48f7627c60a7ded0ea53
Amal Aldawsari <93773622+amalsaud@users.noreply.github.com>
2022-08-20 17:37:18 +0300

    Update PersonCard.jsx

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 class PersonCard extends Component {
     render() {
-        const { fName, lName, age, hairColor } = this.props;
         return (
+            const { fName, lName, age, hairColor } = this.props;
             <div className='card'>
                 <h1>{lName}, {fName}</h1>
                 <p>{age}</p>
